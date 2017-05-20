6398d4484b695f1d9f095af8d953cec1fece987a
0x4164 <adib_okay@yahoo.co.id>
2017-05-20 11:22:16 +0700

    head html up

--- application/views/core/head-html.php
+++ application/views/core/head-html.php
@@ -1,53 +1,78 @@
 <?php
   $title = $page;
-?>
+  // <link href="<?php echo site_url('assets/css/style.css'); " rel="stylesheet">?>
+<?php
+  // <link href="<?php echo base_url('assets/css/bootstrap-responsive.css'); " rel="stylesheet">?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo "-".$title."-" ;?></title>
-    <link href="<?php echo site_url('assets/css/style.css'); ?>" rel="stylesheet">
-  	<link href="<?php echo base_url('assets/css/bootstrap-responsive.css'); ?>" rel="stylesheet">
   	<link href="<?php echo site_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
   	<link href="<?php echo site_url('assets/css/bootstrap.min.css'); ?>" rel="stylesheet">
   	<link href="<?php echo site_url('assets/css/w3.css'); ?>" rel="stylesheet">
-  	<script src="<?php echo site_url('assets/js/jquery-3.2.1.min.js'); ?>"></script>
-  	<script src="<?php echo site_url('assets/js/jquery-ui.min.js'); ?>"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/login-layout.css'); ?>">
+    <script src="<?php echo site_url('assets/js/jquery-3.2.1.min.js');?>"></script>
+    <script src="<?php echo site_url('assets/js/jquery-ui.js'); ?>"></script>
+  	<script src="<?php echo site_url('assets/js/bootstrap.js');?>"></script>
     <style media="screen">
-      /*navbar*/
-      .navbar-default {
-        background-color: #00AA44;
-        border-color: #00AA44;
-      }
-      .navbar-nav .navbar-right .lnk{
-        color: #000000;
-      }
-      .navbar-header .navbar-brand{
-        color: #000000;
-      }
       /*background*/
+      .bg{
+        background-color: rgba(0,0,0,0.1);
+      }
       .bg-cara{
-        background-color: #217844;
-        color: #000000;
+        color: #ffffff;
       }
       .bg-tips{
-        background-color: #00aa44;
-        color: #000000;
+        color: #ffffff;
       }
-      .bg-coba{
-        background-color: #217820;
-        color: #000000;
+      .bg-about{
+        background-color: rgba(0,0,0,0.1);
+        color: #ffffff;
       }
       .pd2em{
-        padding:2em;
+        padding:3em;
       }
       .hg{
         height: 700px;
       }
+      .hg-6{
+        /*height: 600px;*/
+        height: 50%;
+      }
+      .hg-row{
+        height:300px;
+      }
       .hg-about{
         height: 500px;
       }
+      .navbar-header .navbar-brand{
+        color: #00aa44;
+      }
+      .lnk{
+        color: #00aa44;
+      }
+      .vertical-center {
+        min-height: 100%;  /* Fallback for browsers do NOT support vh unit */
+        min-height: 100vh; /* These two lines are counted as one :-)       */
+
+        display: flex;
+        align-items: center;
+      }
+      #testerakhir{
+        height: 300px;
+        background-color: #217844;
+      }
+      #tips{
+        height: 300px;
+        background-color: #217844;
+      }
+      #statistik{
+        height: 300px;
+        background-color: #00aa44;
+      }
     </style>
   </head>
-<body style="background-color: #ffffff; color: #000000;">
-<!-- padding-top: 70px; -->
+  <body style="background-color: #79b79c;">
+  <!-- padding-top: 70px; -->
