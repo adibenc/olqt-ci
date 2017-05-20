14e2cb920e57d43ad74973068f5e2b7a76a6cd5b
0x4164 <adib_okay@yahoo.co.id>
2017-05-20 11:19:53 +0700

    responsive navbar

--- application/views/core/navbar.php
+++ application/views/core/navbar.php
@@ -4,19 +4,26 @@
   updates
   21/4/2017
     - bootstrap navbar
+  20/5/2017
+   - responsive
   */
 ?>
 <!-- navbar -->
+<nav class="col-md-12 navbar navbar-default" style="margin-bottom:50px;">
+  <div class="container-fluid">
 <?php
   if($this->session->userdata('username')&&$this->session->userdata('password')){
 ?>
-<nav class="col-md-12 navbar navbar-default" data-spy="affix">
-  <div class="container-fluid">
     <div class="navbar-header">
       <a class="navbar-brand" href="beranda">OLQT</a>
+      <button type="button" class="navbar-toggle collapsed" name="button" data-toggle="collapse" data-target="#mainnavbar" aria-expanded="false">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
     </div>
-    <div class="collapse navbar-collapse">
-      <ul class="nav navbar-nav navbar-right  ">
+    <div class="collapse navbar-collapse" id="mainnavbar" aria-expanded="false">
+      <ul class="nav navbar-nav navbar-right">
         <li><a class="lnk" href="<?php echo base_url('beranda')?>">Beranda</a></li>
         <li><a class="lnk" href="<?php echo base_url('tes')?>">Tes</a></li>
         <li><a class="lnk" href="<?php echo base_url('bantuan')?>">Bantuan</a></li>
@@ -25,28 +32,35 @@
         <li><a class="lnk" href="<?php echo base_url('masuk/keluar')?>">Keluar</a></li>
       </ul>
     </div>
-  </div>
-</nav>
+  <!-- </div> -->
+<!-- </nav> -->
 <?php
   }else{
 ?>
 <!-- nbnormal -->
-<nav class="navbar navbar-default" data-spy="affix">
-  <div class="container-fluid">
+<!-- <nav class="navbar navbar-default" data-spy="affix"> -->
+  <!-- <div class="container-fluid"> -->
     <div class="navbar-header">
       <a class="navbar-brand" href="<?php echo base_url('beranda');?>">OLQT</a>
+      <button type="button" class="navbar-toggle collapsed" name="button" data-toggle="collapse" data-target="#mainnavbar" aria-expanded="false">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
     </div>
-    <div class="collapse navbar-collapse">
+    <div class="collapse navbar-collapse" id="mainnavbar" aria-expanded="false">
       <ul class="nav navbar-nav navbar-right">
         <li><a href="<?php echo base_url('beranda#tips')?>">Tips</a></li>
         <li><a href="<?php echo base_url('beranda#coba')?>">Coba</a></li>
         <li><a href="<?php echo base_url('beranda#kami')?>">Kami</a></li>
-        <li><a href="<?php echo base_url('daftar')?>"><span class="glyphicon glyphicon-user"></span>Daftar</a></li>
-        <li><a href="<?php echo base_url('masuk')?>">Masuk</a></li>
+        <li><a href="<?php echo base_url('daftar')?>"><span class="glyphicon glyphicon-user"></span> Daftar</a></li>
+        <li><a href="<?php echo base_url('masuk')?>"><span class="glyphicon glyphicon-log-in"></span> Masuk</a></li>
       </ul>
     </div>
   </div>
-</nav>
+</div>
 <?php
   }
 ?>
+</nav>
